5a3bb79cf4cd989def7da4032359896547b414bb
marhatha <puneet.marhatha@gmail.com>
2023-11-07 09:47:39 -0600

    Add files via upload
    First commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import torch
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from trl import SFTTrainer
+
+
+def train():
+    train_dataset = load_dataset("tatsu-lab/alpaca", split="train")
+    tokenizer = AutoTokenizer.from_pretrained("daryl149/llama-2-7b-chat-hf", trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    model = AutoModelForCausalLM.from_pretrained(
+        "daryl149/llama-2-7b-chat-hf", load_in_4bit=True, torch_dtype=torch.float16, device_map="auto", load_in_8bit_fp32_cpu_offload=True
+    )
+    model.resize_token_embeddings(len(tokenizer))
+    model = prepare_model_for_int8_training(model)
+    peft_config = LoraConfig(r=16, lora_alpha=32, lora_dropout=0.05, bias="none", task_type="CAUSAL_LM")
+    model = get_peft_model(model, peft_config)
+    training_args = TrainingArguments(
+        output_dir="llma-finetuned-7b2",
+        per_device_train_batch_size=4,
+        optim="adamw_torch",
+        logging_steps=100,
+        learning_rate=2e-4,
+        fp16=True,
+        warmup_ratio=0.1,
+        lr_scheduler_type="linear",
+        num_train_epochs=1,
+        save_strategy="epoch",
+        push_to_hub=True,
+    )
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_dataset,
+        dataset_text_field="text",
+        max_seq_length=1024,
+        tokenizer=tokenizer,
+        args=training_args,
+        packing=True,
+        peft_config=peft_config,
+    )
+    trainer.train()
+    trainer.push_to_hub()
+
+
+if __name__ == "__main__":
+    train()
